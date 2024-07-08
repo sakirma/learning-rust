f706af11f2c897af60e0a1478dc533908f108811
huseyincaliskan <14217365+sakirma@users.noreply.github.com>
2024-07-08 23:01:32 +0200

    rusting for fun

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::collections::HashMap;
 
+pub fn num_water_bottles(num_bottles: i32, num_exchange: i32) -> i32 {
+  let mut drinked = num_bottles;
+  let mut remaining = num_bottles;
+
+  while remaining >= num_exchange {
+    let exchange = remaining / num_exchange;
+    drinked += exchange;
+    remaining = exchange + (remaining % num_exchange);
+  }
+
+
+  return drinked
+}
+
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
   let mut hm = HashMap::with_capacity(nums.len());
   for (i, &num) in nums.iter().enumerate() {
@@ -15,9 +29,11 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
 
 fn main() {
-  let vector: Vec<i32> = two_sum(vec![8, 10, 2], 12);
+  // let vector: Vec<i32> = two_sum(vec![8, 10, 2], 12);
 
-  println!("{:?}", vector)
+  let water_bottles = num_water_bottles(9,3);
+
+  println!("{:?}", water_bottles)
 }
 
 
